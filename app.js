804b2fa20b95d3c7685f494f6efc689bd07e5da8
Sara Sanchez <saralesan@hotmail.com>
2020-04-18 00:20:23 -0500

    API configuration

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const configMessage = require('./configMessage');
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+app.post('/formulario’', (req, res) => {
+    configMensaje(req.body);
+    res.status(200).send();
+})
+
+app.listen(3000, () => {
+    console.log('Servidor corriendo')
+});
